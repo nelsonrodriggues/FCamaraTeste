2e02cf6323b8c01b384600d95858566394b06d34
nelsonrodriggues <nelsonrodriggues@gmail.com>
2017-06-22 06:17:08 -0300

    Add files via upload

---
+++ TesteNelsonClient/TesteNelson.Web.Client/app/controllers/productsController.js
@@ -0,0 +1,11 @@
+﻿app.controller('productsController', ['$http', function ($http) {
+    var self = this;
+
+    self.items = [];
+
+    $http.get('http://localhost:59975/api/products/').then(function (response) {
+        self.items = response.data;
+    }, function (error) {
+        alert('Erro reportado: ' + error);
+    });
+}]);
